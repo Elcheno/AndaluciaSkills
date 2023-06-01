6184b0423528df3af168becbc3c1ae59532b09d7
Ruben <rubenca2312@gmail.com>
2023-06-01 23:11:15 +0200

    push:1.1

--- js/functions.js
+++ js/functions.js
@@ -21,15 +21,37 @@ const createTask = (task, parentNode) => {
     let divTask = document.createElement('div');
     let divTitle = document.createElement('div');
     let titleTask = document.createElement('p');
+    let divDesc = document.createElement('div');
+    let descTask = document.createElement('p');
+    let divBottom = document.createElement('div');
+    let divDateTask = document.createElement('div');
+    let divBtnDone = document.createElement('div');
+    let dateTask = document.createElement('p');
+    let btnTask = document.createElement('p');
 
     titleTask.innerHTML = task.title;
+    descTask.innerHTML = task.description;
+    dateTask.innerHTML = 'FECHA';
+    btnTask.innerHTML = 'BUTTONS'
 
     divTask.classList.add('task');
     divTitle.classList.add('titleTask');
+    divDesc.classList.add('descTask');
+    divBottom.classList.add('bottomTask');
+    divDateTask.classList.add('dateTask');
+    divBtnDone.classList.add('btn-done-Task');
 
     divTask.appendChild(divTitle);
+    divTask.appendChild(divDesc);
+    divTask.appendChild(divBottom);
     divTitle.appendChild(titleTask);
+    divDesc.appendChild(descTask);
+    divBottom.appendChild(divDateTask);
+    divBottom.appendChild(divBtnDone);
+    divDateTask.appendChild(dateTask);
+    divBtnDone.appendChild(btnTask);
     parentNode.appendChild(divTask);
+
 }
 
 const clearField = () => {
